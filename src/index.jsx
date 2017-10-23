e0db1805a65b490f6a6517888862755907b26026
ZitRos <zitros.lab@gmail.com>
2017-10-23 17:51:12 +0300

    [New] Basic input form for package name

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,30 @@ import ReactDom from "react-dom";
 import App from "./react/App.jsx";
 import domLoaded from "dom-loaded";
 
-domLoaded.then(() => {
+function initialize () {
 	ReactDom.render(<App/>, document.getElementById("root"));
+}
+
+domLoaded.then(() => {
+
+	if (location.hash) {
+		return initialize();
+	}
+
+	const form = document.getElementById("welcome-form");
+
+	form.addEventListener("submit", (event) => {
+
+		event.preventDefault();
+
+		const packageName = form.elements["package-name"].value;
+
+		if (!packageName)
+			return;
+
+		location.hash = packageName;
+		initialize();
+
+	});
+
 });
